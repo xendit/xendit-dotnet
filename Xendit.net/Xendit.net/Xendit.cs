f4af1e07724eae666d3f0c0c8978212253e9bcef
Shafiya Adzhani <adz.arsym@gmail.com>
2021-07-07 15:19:23 +0800

    feat: add xendit and balance class

---
+++ Xendit.net/Xendit.net/Xendit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace xendit.net
+{
+    public class Xendit
+    {
+        public static readonly string LiveUrl = "https://api.xendit.co";
+
+        public static volatile string apiKey;
+
+        private static volatile string apiUrl = LiveUrl;
+
+        public static string ApiUrl { get;  }
+
+        public static string ApiKey { get; }
+    }
+}
