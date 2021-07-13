6c55ba8aaecb853880236f38e9e532c2453c87a7
Shafiya Adzhani <adz.arsym@gmail.com>
2021-07-13 16:25:13 +0800

    feat(virtual account): add get available banks

---
+++ Xendit.net/Xendit.net/Model/AvailableBank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Xendit.net.Model
+{
+    public class AvailableBank
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+    }
+}
---
+++ Xendit.net/Xendit.net/Model/VirtualAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Xendit.net.Model
+{
+    public class VirtualAccount
+    {
+        public static Task<List<AvailableBank>> GetAvailableBanks()
+        {
+            return GetAvailableBanks(new Dictionary<string, string>());
+        }
+
+        public static async Task<List<AvailableBank>> GetAvailableBanks(Dictionary<string, string> headers)
+        {
+            string url = string.Format("{0}{1}", XenditConfiguration.ApiUrl, "/available_virtual_account_banks");
+
+            var result = await XenditConfiguration.requestClient.Request(headers, url);
+            var availableBanks = await JsonSerializer.DeserializeAsync<List<AvailableBank>>(result);
+            return availableBanks;
+        }
+    }
+}
