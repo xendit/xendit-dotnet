594be73109472305523cd7006a5e64181a0f879c
Shafiya Adzhani <adz.arsym@gmail.com>
2021-07-14 16:16:07 +0800

    docs(example): Add example for get balance

---
+++ XenditExample/ExampleGetBalance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Xendit.net.Model;
+using Xendit.net;
+using Xendit.net.Network;
+
+namespace XenditExample
+{
+    class ExampleGetBalance
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            NetworkClient networkClient = new NetworkClient(httpClient);
+            XenditConfiguration.RequestClient = networkClient;
+            XenditConfiguration.ApiKey = "xnd_development_...";
+            
+            Balance balance = await Balance.Get();
+            Console.WriteLine(balance.Value);
+        }
+    }
+}
