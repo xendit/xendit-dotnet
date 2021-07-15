3d7d9c04731e62b1b870cb6be92d556dfc97d59c
Shafiya Adzhani <adz.arsym@gmail.com>
2021-07-16 04:29:08 +0800

    test(balance): add tests for balance
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ Xendit.net/XenditTest/BalanceTest.cs
@@ -0,0 +1,49 @@
+using Xunit;
+using Moq;
+using Xendit.net.Model;
+using Xendit.net.Network;
+using Xendit.net;
+using System.Net.Http;
+using System.Collections.Generic;
+
+namespace XenditTest
+{
+    public class BalanceTest
+    {
+        private readonly Mock<INetworkClient> mockClient = new Mock<INetworkClient>();
+        private readonly string url = "https://api.xendit.co/balance";
+
+        [Fact]
+        public async void Balance_ShouldSuccess_IfNoGivenParam()
+        {
+            Balance expectedBalance = new Balance();
+            expectedBalance.Value = 10000;
+
+            mockClient
+                .Setup(x => x.Request<Balance>(HttpMethod.Get, new Dictionary<string, string>(), url, It.IsAny<Dictionary<string, object>>()))
+                .ReturnsAsync(() => expectedBalance);
+
+            XenditConfiguration.RequestClient = mockClient.Object;
+
+            Balance actualBalance = await Balance.Get();
+            Assert.Equal(expectedBalance, actualBalance);
+        }
+
+        [Fact]
+        public async void Balance_ShouldSuccess_IfGivenParam()
+        {
+            Balance expectedBalance = new Balance();
+            expectedBalance.Value = 10000;
+
+            string accountTypeUrl = string.Format("{0}?account_type={1}", url, "HOLDING");
+
+            mockClient
+                .Setup(x => x.Request<Balance>(HttpMethod.Get, new Dictionary<string, string>(), accountTypeUrl, It.IsAny<Dictionary<string, object>>()))
+                .ReturnsAsync(expectedBalance);
+            XenditConfiguration.RequestClient = mockClient.Object;
+
+            Balance actualBalance = await Balance.Get(accountType: Balance.AccountType.Holding);
+            Assert.Equal(expectedBalance, actualBalance);
+        }
+    }
+}
