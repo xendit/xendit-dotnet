cfe40690bed6b61ec753307e744ef52d66eda72c
Shafiya Adzhani <adz.arsym@gmail.com>
2021-07-16 06:05:59 +0800

    test(balance): add tests for get balance with custom headers and account type string
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

--- Xendit.net/XenditTest/BalanceTest.cs
+++ Xendit.net/XenditTest/BalanceTest.cs
@@ -5,6 +5,9 @@ using Xendit.net.Network;
 using Xendit.net;
 using System.Net.Http;
 using System.Collections.Generic;
+using System.Text.Json;
+using Xendit.net.Exception;
+using System.Threading.Tasks;
 
 namespace XenditTest
 {
@@ -13,6 +16,10 @@ namespace XenditTest
         private readonly Mock<INetworkClient> mockClient = new Mock<INetworkClient>();
         private readonly string url = "https://api.xendit.co/balance";
         private readonly Balance expectedBalance = new Balance { Value = 10000 };
+        private readonly Dictionary<string, string> customHeaders = new Dictionary<string, string>()
+        {
+            { "for-user-id", "user-id" }
+        };
 
         [Fact]
         public async void Balance_ShouldSuccess_IfNoGivenParam()
@@ -24,7 +31,7 @@ namespace XenditTest
             XenditConfiguration.RequestClient = mockClient.Object;
 
             Balance actualBalance = await Balance.Get();
-            Assert.Equal(expectedBalance, actualBalance);
+            Assert.Equal(JsonSerializer.Serialize(expectedBalance), JsonSerializer.Serialize(actualBalance));
         }
 
         [Fact]
@@ -38,7 +45,61 @@ namespace XenditTest
             XenditConfiguration.RequestClient = mockClient.Object;
 
             Balance actualBalance = await Balance.Get(accountType: Balance.AccountType.Holding);
-            Assert.Equal(expectedBalance, actualBalance);
+            Assert.Equal(JsonSerializer.Serialize(expectedBalance), JsonSerializer.Serialize(actualBalance));
+        }
+
+        [Fact]
+        public async void Balance_ShouldSuccess_IfGivenParam_WithCustomHeaders()
+        {
+            string accountTypeUrl = string.Format("{0}?account_type={1}", url, "HOLDING");
+
+            mockClient
+                .Setup(client => client.Request<Balance>(HttpMethod.Get, customHeaders, accountTypeUrl, null))
+                .ReturnsAsync(expectedBalance);
+            XenditConfiguration.RequestClient = mockClient.Object;
+
+            Balance actualBalance = await Balance.Get(headers: customHeaders, accountType: Balance.AccountType.Holding);
+            Assert.Equal(JsonSerializer.Serialize(expectedBalance), JsonSerializer.Serialize(actualBalance));
+        }
+
+        [Fact]
+        public async void Balance_ShouldSuccess_IfGivenParamAccountTypeString()
+        {
+            string accountTypeUrl = string.Format("{0}?account_type={1}", url, "HOLDING");
+
+            mockClient
+                .Setup(client => client.Request<Balance>(HttpMethod.Get, new Dictionary<string, string>(), accountTypeUrl, null))
+                .ReturnsAsync(expectedBalance);
+            XenditConfiguration.RequestClient = mockClient.Object;
+
+            Balance actualBalance = await Balance.Get(accountTypeValue: "Holding");
+            Assert.Equal(JsonSerializer.Serialize(expectedBalance), JsonSerializer.Serialize(actualBalance));
+        }
+
+        [Fact]
+        public async Task Balance_ShouldFail_IfAccountTypeStringDoesntExist()
+        {
+            await Assert.ThrowsAsync<ParamException>(() => Balance.Get(accountTypeValue: "Wallet"));
+        }
+
+        [Fact]
+        public async void Balance_ShouldSuccess_IfGivenParamAccountTypeString_WithCustomHeaders()
+        {
+            string accountTypeUrl = string.Format("{0}?account_type={1}", url, "HOLDING");
+
+            mockClient
+                .Setup(client => client.Request<Balance>(HttpMethod.Get, customHeaders, accountTypeUrl, null))
+                .ReturnsAsync(expectedBalance);
+            XenditConfiguration.RequestClient = mockClient.Object;
+
+            Balance actualBalance = await Balance.Get(headers: customHeaders, accountTypeValue: "Holding");
+            Assert.Equal(JsonSerializer.Serialize(expectedBalance), JsonSerializer.Serialize(actualBalance));
+        }
+
+        [Fact]
+        public async Task Balance_ShouldFail_IfAccountTypeStringDoesntExist_WithCustomHeaders()
+        {
+            await Assert.ThrowsAsync<ParamException>(() => Balance.Get(headers: customHeaders, accountTypeValue: "Wallet"));
         }
     }
 }
