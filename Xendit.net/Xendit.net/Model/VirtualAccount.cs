2ebabf54f66f2dc8b6552ef65d8b5e197acbf79e
Shafiya Adzhani <adz.arsym@gmail.com>
2021-07-16 13:05:21 +0800

    feat(virtual account): separate create VA into closed and open VA
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

--- Xendit.net/Xendit.net/Model/VirtualAccount.cs
+++ Xendit.net/Xendit.net/Model/VirtualAccount.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Xendit.net.Exception;
 
 namespace Xendit.net.Model
 {
@@ -43,6 +44,18 @@ namespace Xendit.net.Model
         [JsonPropertyName("status")]
         public string Status { get; set; }
 
+        [JsonPropertyName("currency")]
+        public string Currency { get; set; }
+
+        [JsonPropertyName("expected_amount")]
+        public int ExpectedAmount { get; set; }
+
+        [JsonPropertyName("suggested_amount")]
+        public int SuggestedAmount { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
         public static Task<List<AvailableBank>> GetAvailableBanks()
         {
             return GetAvailableBanks(new Dictionary<string, string>());
@@ -69,14 +82,118 @@ namespace Xendit.net.Model
             return virtualAccount;
         }
 
-        public static Task<VirtualAccount> Create(Dictionary<string, object> parameter)
+        public static Task<VirtualAccount> CreateClosed(Dictionary<string, object> parameter)
         {
-            return Create(new Dictionary<string, string>(), parameter);
+            return Create(new Dictionary<string, string>(), parameter, true);
         }
 
-        public static async Task<VirtualAccount> Create(Dictionary<string, string> headers, Dictionary<string, object> parameter)
+        public static Task<VirtualAccount> CreateClosed(Dictionary<string, string> headers, Dictionary<string, object> parameter)
+        {
+            return Create(headers, parameter, true);
+        }
+
+        public static Task<VirtualAccount> CreateClosed(string externalId, string bankCode, string name)
+        {
+            Dictionary<string, object> parameter = new Dictionary<string, object>()
+            {
+                { "external_id", externalId },
+                { "bank_code", bankCode },
+                { "name", name },
+            };
+
+            return Create(new Dictionary<string, string>(), parameter, true);
+        }
+        public static Task<VirtualAccount> CreateClosed(Dictionary<string, string> headers, string externalId, string bankCode, string name)
+        {
+            Dictionary<string, object> parameter = new Dictionary<string, object>()
+            {
+                { "external_id", externalId },
+                { "bank_code", bankCode },
+                { "name", name },
+            }; 
+
+            return Create(headers, parameter, true);
+        }
+
+        public static Task<VirtualAccount> CreateClosed(string externalId, string bankCode, string name, Dictionary<string, object> parameter)
+        {
+            parameter.Add("external_id", externalId);
+            parameter.Add("bank_code", bankCode);
+            parameter.Add("name", name);
+
+            return Create(new Dictionary<string, string>(), parameter, true);
+        }
+
+        public static Task<VirtualAccount> CreateClosed(Dictionary<string, string> headers, string externalId, string bankCode, string name, Dictionary<string, object> parameter)
+        {
+            parameter.Add("external_id", externalId);
+            parameter.Add("bank_code", bankCode);
+            parameter.Add("name", name);
+
+            return Create(headers, parameter, true);
+        }
+
+        public static Task<VirtualAccount> CreateOpen(Dictionary<string, object> parameter)
+        {
+            return Create(new Dictionary<string, string>(), parameter, false);
+        }
+
+        public static Task<VirtualAccount> CreateOpen(Dictionary<string, string> headers, Dictionary<string, object> parameter)
+        {
+            return Create(headers, parameter, false);
+        }
+
+        public static Task<VirtualAccount> CreateOpen(string externalId, string bankCode, string name)
+        {
+            Dictionary<string, object> parameter = new Dictionary<string, object>()
+            {
+                { "external_id", externalId },
+                { "bank_code", bankCode },
+                { "name", name },
+            };
+
+            return Create(new Dictionary<string, string>(), parameter, false);
+        }
+        public static Task<VirtualAccount> CreateOpen(Dictionary<string, string> headers, string externalId, string bankCode, string name)
+        {
+            Dictionary<string, object> parameter = new Dictionary<string, object>()
+            {
+                { "external_id", externalId },
+                { "bank_code", bankCode },
+                { "name", name },
+            };
+
+            return Create(headers, parameter, false);
+        }
+
+        public static Task<VirtualAccount> CreateOpen(string externalId, string bankCode, string name, Dictionary<string, object> parameter)
+        {
+            parameter.Add("external_id", externalId);
+            parameter.Add("bank_code", bankCode);
+            parameter.Add("name", name);
+
+            return Create(new Dictionary<string, string>(), parameter, false);
+        }
+
+        public static Task<VirtualAccount> CreateOpen(Dictionary<string, string> headers, string externalId, string bankCode, string name, Dictionary<string, object> parameter)
+        {
+            parameter.Add("external_id", externalId);
+            parameter.Add("bank_code", bankCode);
+            parameter.Add("name", name);
+
+            return Create(headers, parameter, false);
+        }
+
+        private static async Task<VirtualAccount> Create(Dictionary<string, string> headers, Dictionary<string, object> parameter, bool isClosed)
         {
             string url = string.Format("{0}{1}", XenditConfiguration.ApiUrl, "/callback_virtual_accounts");
+
+            parameter.Add("is_closed", isClosed);
+
+            if (isClosed && parameter.ContainsKey("suggested_amount")) 
+            {
+                throw new ParamException("Suggested amount is not supported for closed VA");
+            }
 
             var virtualAccount = await XenditConfiguration.RequestClient.Request<VirtualAccount>(HttpMethod.Post, headers, url, parameter);
             return virtualAccount;
