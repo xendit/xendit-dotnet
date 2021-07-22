8b2f194ec235bc1890f5b5dfabcd0161a93a54aa
Shafiya Adzhani <adz.arsym@gmail.com>
2021-07-22 08:01:03 +0800

    docs(virtual account): add virtual account, payment, available banks example
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ XenditExample/ExampleUpdateVirtualAccount.cs
@@ -0,0 +1,34 @@
+namespace XenditExample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Net.Http;
+    using Xendit.net;
+    using Xendit.net.Exception;
+    using Xendit.net.Model;
+    using Xendit.net.Network;
+
+    class ExampleUpdateVirtualAccount
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            NetworkClient networkClient = new NetworkClient(httpClient);
+            XenditConfiguration.RequestClient = networkClient;
+            XenditConfiguration.ApiKey = "xnd_development_...";
+
+            try
+            {
+                Dictionary<string, object> parameter = new Dictionary<string, object>();
+                parameter.Add("is_single_use", true);
+
+                VirtualAccount virtualAccount = await VirtualAccount.Update("5e2fd16370279c782d5f56a3", parameter);
+                Console.WriteLine(virtualAccount.IsSingleUse);
+            }
+            catch (XenditException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
