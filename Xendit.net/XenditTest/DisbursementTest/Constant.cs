74a06509f3028ad5a1e0a7d5919f827636dc8ade
Shafiya Adzhani <adz.arsym@gmail.com>
2021-07-27 07:23:07 +0800

    test(disbursement): add test for get available banks, get by id, get by external id
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ Xendit.net/XenditTest/DisbursementTest/Constant.cs
@@ -0,0 +1,44 @@
+﻿namespace XenditTest.DisbursementTest
+{
+    using System.Collections.Generic;
+    using Xendit.net.Model;
+
+    internal static class Constant
+    {
+        internal static readonly AvailableBank[] ExpectedAvailableBanks =
+        {
+            new AvailableBank { Name = "Bank Negara Indonesia", Code = "BNI", CanDisburse = true, CanNameValidate = true },
+            new AvailableBank { Name = "Bank Rakyat Indonesia", Code = "BRI", CanDisburse = true, CanNameValidate = true },
+        };
+
+        internal static readonly Disbursement ExpectedDisbursement = new Disbursement
+        {
+            Id = "57f1ce05bb1a631a65eee662",
+            ExternalId = "disb-1475459775872",
+            UserId = "5785e6334d7b410667d355c4",
+            BankCode = "BCA",
+            AccountHolderName = "MICHAEL CHEN",
+            Amount = 90000,
+            DisbursementDescription = "Reimbursement for shoes",
+            Status = "PENDING",
+            EmailTo = new string[] { "somebody@email.com" },
+            EmailCC = new string[] { "somebody.else@gmail.com" },
+            EmailBCC = new string[] { "someone@mail.com" },
+        };
+
+        internal static readonly Disbursement[] ExpectedDisbursements = new Disbursement[] { ExpectedDisbursement };
+
+        internal static readonly string ExpectedDisbursementId = "57f1ce05bb1a631a65eee662";
+        internal static readonly string ExpectedDisbursementExternalId = "disb-1475459775872";
+
+        internal static readonly string DisbursementIdUrl = string.Format("https://api.xendit.co/disbursements/{0}", ExpectedDisbursementId);
+        internal static readonly string DisbursementExternalIdUrl = string.Format("https://api.xendit.co/disbursements?external_id={0}", ExpectedDisbursementExternalId);
+
+        internal static readonly Dictionary<string, string> CustomHeaders = new Dictionary<string, string>()
+        {
+            { "for-user-id", "user-id" },
+        };
+
+        internal static readonly string AvailableBankUrl = "https://api.xendit.co/available_disbursements_banks";
+    }
+}
---
+++ Xendit.net/XenditTest/DisbursementTest/DisbursementTest.cs
@@ -0,0 +1,94 @@
+﻿namespace XenditTest.DisbursementTest
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text.Json;
+    using Moq;
+    using Xendit.net;
+    using Xendit.net.Model;
+    using Xendit.net.Network;
+    using Xunit;
+
+    public class DisbursementTest
+    {
+        private static readonly Mock<INetworkClient> MockClient = new Mock<INetworkClient>();
+
+        [Fact]
+        public async void Disbursement_ShouldSuccess_GetAvailableBanks()
+        {
+            MockClient
+                .Setup(client => client.Request<AvailableBank[]>(HttpMethod.Get, new Dictionary<string, string>(), Constant.AvailableBankUrl, null))
+                .ReturnsAsync(Constant.ExpectedAvailableBanks);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            AvailableBank[] actualAvailableBanks = await Disbursement.GetAvailableBanks();
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedAvailableBanks), JsonSerializer.Serialize(actualAvailableBanks));
+        }
+
+        [Fact]
+        public async void Disbursement_ShouldSuccess_GetAvailableBanks_WithCustomHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<AvailableBank[]>(HttpMethod.Get, Constant.CustomHeaders, Constant.AvailableBankUrl, null))
+                .ReturnsAsync(Constant.ExpectedAvailableBanks);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            AvailableBank[] actualAvailableBanks = await Disbursement.GetAvailableBanks(Constant.CustomHeaders);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedAvailableBanks), JsonSerializer.Serialize(actualAvailableBanks));
+        }
+
+        [Fact]
+        public async void Disbursement_ShouldSuccess_GetById()
+        {
+            MockClient
+                .Setup(client => client.Request<Disbursement>(HttpMethod.Get, new Dictionary<string, string>(), Constant.DisbursementIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedDisbursement);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Disbursement actualDisbursement = await Disbursement.GetById(Constant.ExpectedDisbursementId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedDisbursement), JsonSerializer.Serialize(actualDisbursement));
+        }
+
+        [Fact]
+        public async void Disbursement_ShouldSuccess_GetById_WithCustomHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<Disbursement>(HttpMethod.Get, Constant.CustomHeaders, Constant.DisbursementIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedDisbursement);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Disbursement actualDisbursement = await Disbursement.GetById(Constant.CustomHeaders, Constant.ExpectedDisbursementId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedDisbursement), JsonSerializer.Serialize(actualDisbursement));
+        }
+
+        [Fact]
+        public async void Disbursement_ShouldSuccess_GetByExternalId()
+        {
+            MockClient
+                .Setup(client => client.Request<Disbursement[]>(HttpMethod.Get, new Dictionary<string, string>(), Constant.DisbursementExternalIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedDisbursements);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Disbursement[] actualDisbursements = await Disbursement.GetByExternalId(Constant.ExpectedDisbursementExternalId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedDisbursements), JsonSerializer.Serialize(actualDisbursements));
+        }
+
+        [Fact]
+        public async void Disbursement_ShouldSuccess_GetByExternalId_WithCustomHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<Disbursement[]>(HttpMethod.Get, Constant.CustomHeaders, Constant.DisbursementExternalIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedDisbursements);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Disbursement[] actualDisbursements = await Disbursement.GetByExternalId(Constant.CustomHeaders, Constant.ExpectedDisbursementExternalId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedDisbursements), JsonSerializer.Serialize(actualDisbursements));
+        }
+    }
+}
