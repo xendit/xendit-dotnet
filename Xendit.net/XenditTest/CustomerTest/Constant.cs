8b5946bba2fbf2cd212b93cd109db209f91f8a51
Shafiya Adzhani <adz.arsym@gmail.com>
2021-08-08 05:19:17 +0800

    test(customer): add test for customer
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ Xendit.net/XenditTest/CustomerTest/Constant.cs
@@ -0,0 +1,43 @@
+﻿namespace XenditTest.CustomerTest
+{
+    using System.Collections.Generic;
+    using Xendit.net.Model;
+
+    internal class Constant
+    {
+        internal static readonly Dictionary<string, string> CustomHeaders = new Dictionary<string, string>()
+        {
+            { "for-user-id", "user-id" },
+        };
+
+        internal static readonly string CustomerUrl = "https://api.xendit.co/customers";
+
+        internal static readonly Customer ExpectedCustomer = new Customer
+        {
+            Id = "239c16f4-866d-43e8-9341-7badafbc019f",
+            ReferenceId = "demo_1475801962607",
+            Email = "customer@website.com",
+            MobileNumber = "+6287774441111",
+            GivenNames = "John",
+            Description = "New Customer",
+            MiddleName = null,
+            Surname = null,
+            PhoneNumber = null,
+            Nationality = null,
+            Addresses = null,
+            DateOfBirth = null,
+            Metadata = null,
+        };
+
+        internal static readonly Dictionary<string, object> CustomerBody = new Dictionary<string, object>()
+        {
+            { "reference_id", "demo_1475801962607" },
+            { "given_names", "John" },
+            { "mobile_number", "+6287774441111" },
+            { "email", "customer@website.com" },
+        };
+
+        internal static readonly Customer[] ExpectedCustomers = new Customer[] { ExpectedCustomer };
+        internal static readonly string CustomerIdUrl = string.Format("{0}?reference_id={1}", CustomerUrl, ExpectedCustomer.ReferenceId);
+    }
+}
---
+++ Xendit.net/XenditTest/CustomerTest/CustomerTest.cs
@@ -0,0 +1,94 @@
+﻿namespace XenditTest.CustomerTest
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text.Json;
+    using Moq;
+    using Xendit.net;
+    using Xendit.net.Model;
+    using Xendit.net.Network;
+    using Xunit;
+
+    public class CustomerTest
+    {
+        private static readonly Mock<INetworkClient> MockClient = new Mock<INetworkClient>();
+
+        [Fact]
+        public async void Customer_ShouldSuccess_CreateDictionaryParameters()
+        {
+            MockClient
+                .Setup(client => client.Request<Customer>(HttpMethod.Post, new Dictionary<string, string>(), Constant.CustomerUrl, Constant.CustomerBody))
+                .ReturnsAsync(Constant.ExpectedCustomer);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Customer actualCustomer = await Customer.Create(Constant.CustomerBody);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedCustomer), JsonSerializer.Serialize(actualCustomer));
+        }
+
+        [Fact]
+        public async void Customer_ShouldSuccess_CreateDictionaryParameters_WithHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<Customer>(HttpMethod.Post, Constant.CustomHeaders, Constant.CustomerUrl, Constant.CustomerBody))
+                .ReturnsAsync(Constant.ExpectedCustomer);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Customer actualCustomer = await Customer.Create(Constant.CustomHeaders, Constant.CustomerBody);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedCustomer), JsonSerializer.Serialize(actualCustomer));
+        }
+
+        [Fact]
+        public async void Customer_ShouldSuccess_CreateRequiredParams()
+        {
+            MockClient
+                .Setup(client => client.Request<Customer>(HttpMethod.Post, new Dictionary<string, string>(), Constant.CustomerUrl, Constant.CustomerBody))
+                .ReturnsAsync(Constant.ExpectedCustomer);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Customer actualCustomer = await Customer.Create(Constant.ExpectedCustomer.ReferenceId, Constant.ExpectedCustomer.GivenNames, Constant.ExpectedCustomer.MobileNumber, Constant.ExpectedCustomer.Email);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedCustomer), JsonSerializer.Serialize(actualCustomer));
+        }
+
+        [Fact]
+        public async void Customer_ShouldSuccess_CreateRequiredParams_WithHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<Customer>(HttpMethod.Post, Constant.CustomHeaders, Constant.CustomerUrl, Constant.CustomerBody))
+                .ReturnsAsync(Constant.ExpectedCustomer);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Customer actualCustomer = await Customer.Create(Constant.CustomHeaders, Constant.ExpectedCustomer.ReferenceId, Constant.ExpectedCustomer.GivenNames, Constant.ExpectedCustomer.MobileNumber, Constant.ExpectedCustomer.Email);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedCustomer), JsonSerializer.Serialize(actualCustomer));
+        }
+
+        [Fact]
+        public async void Customer_ShouldSuccess_GetByReferenceId()
+        {
+            MockClient
+                .Setup(client => client.Request<Customer[]>(HttpMethod.Get, new Dictionary<string, string>(), Constant.CustomerIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedCustomers);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Customer[] actualCustomers = await Customer.GetByReferenceId(Constant.ExpectedCustomer.ReferenceId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedCustomers), JsonSerializer.Serialize(actualCustomers));
+        }
+
+        [Fact]
+        public async void Customer_ShouldSuccess_GetByReferenceId_WithHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<Customer[]>(HttpMethod.Get, Constant.CustomHeaders, Constant.CustomerIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedCustomers);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            Customer[] actualCustomers = await Customer.GetByReferenceId(Constant.CustomHeaders, Constant.ExpectedCustomer.ReferenceId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedCustomers), JsonSerializer.Serialize(actualCustomers));
+        }
+    }
+}
