e2789b803b3ba22364bf73eed30bea4f1c79fd85
Shafiya Adzhani <adz.arsym@gmail.com>
2021-08-13 17:17:23 +0800

    test(ewallets): add ewallet test
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ Xendit.net/XenditTest/EWalletChargeTest/Constant.cs
@@ -0,0 +1,79 @@
+﻿namespace XenditTest.EWalletChargeTest
+{
+    using System.Collections.Generic;
+    using Xendit.net.Model;
+
+    internal class Constant
+    {
+        internal static readonly EWalletCharge ExpectedEWalletCharge = new EWalletCharge
+        {
+            Id = "charge-id",
+            BusinessId = "5f218745736e619164dc8608",
+            ReferenceId = "test-reference-id",
+            Status = "PENDING",
+            Currency = "IDR",
+            ChargeAmount = 1000,
+            CaptureAmount = 1000,
+            CheckoutMethod = "ONE_TIME_PAYMENT",
+            ChannelCode = "ID_SHOPEEPAY",
+            ChannelProperties = new Dictionary<string, string>()
+            {
+                { "success_redirect_url",  "https://dashboard.xendit.co/register/1" },
+            },
+            Actions = new Dictionary<string, string>()
+            {
+                { "desktop_web_checkout_url",  "https://dashboard.xendit.co/register/1" },
+                { "mobile_web_checkout_url", null },
+                { "mobile_deeplink_checkout_url",  "https://deeplinkcheckout.this/" },
+                { "qr_checkout_string", "ID123XenditQRTest321DI" },
+            },
+            IsRedirectRequired = true,
+            CallbackUrl = "https://calling-back.com/xendit/shopeepay",
+            Created = "2017-07-21T17:32:28Z",
+            Updated = "2017-07-21T17:32:28Z",
+            VoidedAt = null,
+            CaptureNow = true,
+            CustomerId = null,
+            PaymentMethodId = null,
+            FailureCode = null,
+            Basket = null,
+            Metadata = new Dictionary<string, object>()
+            {
+                { "branch_code", "tree_branch" },
+            },
+        };
+
+        internal static readonly string ReferenceId = "test-reference-id";
+        internal static readonly string Currency = "IDR";
+        internal static readonly long Amount = 1000;
+        internal static readonly string CheckoutMethod = "ONE_TIME_PAYMENT";
+        internal static readonly string ChannelCode = "ID_SHOPEEPAY";
+        internal static readonly Dictionary<string, string> ChannelProperties = new Dictionary<string, string>()
+        {
+            { "success_redirect_url",  "https://dashboard.xendit.co/register/1" },
+        };
+
+        internal static readonly Dictionary<string, object> EWalletBody = new Dictionary<string, object>()
+        {
+            { "reference_id", ReferenceId },
+            { "currency", Currency },
+            { "amount", Amount },
+            { "checkout_method", CheckoutMethod },
+            { "channel_code", ChannelCode },
+            { "channel_properties", ChannelProperties },
+            { "payment_method_id", null },
+            { "customer_id", null },
+            { "basket", null },
+            { "metadata", null },
+        };
+
+        internal static readonly Dictionary<string, string> CustomHeaders = new Dictionary<string, string>()
+        {
+            { "for-user-id", "user-id" },
+        };
+
+        internal static readonly string ChargeId = "charge-id";
+        internal static readonly string EWalletChargeUrl = "https://api.xendit.co/ewallets/charges";
+        internal static readonly string GetChargeUrl = string.Format("{0}/{1}", EWalletChargeUrl, ChargeId);
+    }
+}
---
+++ Xendit.net/XenditTest/EWalletChargeTest/EWalletChargeTest.cs
@@ -0,0 +1,94 @@
+﻿namespace XenditTest.EWalletChargeTest
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text.Json;
+    using Moq;
+    using Xendit.net;
+    using Xendit.net.Model;
+    using Xendit.net.Network;
+    using Xunit;
+
+    public class EWalletChargeTest
+    {
+        private static readonly Mock<INetworkClient> MockClient = new Mock<INetworkClient>();
+
+        [Fact]
+        public async void EWalletCharge_ShouldSuccess_GetByChargeId()
+        {
+            MockClient
+                .Setup(client => client.Request<EWalletCharge>(HttpMethod.Get, new Dictionary<string, string>(), Constant.GetChargeUrl, null))
+                .ReturnsAsync(Constant.ExpectedEWalletCharge);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            EWalletCharge actualEWalletCharge = await EWalletCharge.Get(Constant.ChargeId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedEWalletCharge), JsonSerializer.Serialize(actualEWalletCharge));
+        }
+
+        [Fact]
+        public async void EWalletCharge_ShouldSuccess_GetByChargeId_WithHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<EWalletCharge>(HttpMethod.Get, Constant.CustomHeaders, Constant.GetChargeUrl, null))
+                .ReturnsAsync(Constant.ExpectedEWalletCharge);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            EWalletCharge actualEWalletCharge = await EWalletCharge.Get(Constant.CustomHeaders, Constant.ChargeId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedEWalletCharge), JsonSerializer.Serialize(actualEWalletCharge));
+        }
+
+        [Fact]
+        public async void EWalletCharge_ShouldSuccess_Create()
+        {
+            MockClient
+                .Setup(client => client.Request<EWalletCharge>(HttpMethod.Post, new Dictionary<string, string>(), Constant.EWalletChargeUrl, Constant.EWalletBody))
+                .ReturnsAsync(Constant.ExpectedEWalletCharge);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            EWalletCharge actualEWalletCharge = await EWalletCharge.Create(Constant.EWalletBody);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedEWalletCharge), JsonSerializer.Serialize(actualEWalletCharge));
+        }
+
+        [Fact]
+        public async void EWalletCharge_ShouldSuccess_Create_WithHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<EWalletCharge>(HttpMethod.Post, Constant.CustomHeaders, Constant.EWalletChargeUrl, Constant.EWalletBody))
+                .ReturnsAsync(Constant.ExpectedEWalletCharge);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            EWalletCharge actualEWalletCharge = await EWalletCharge.Create(Constant.CustomHeaders, Constant.EWalletBody);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedEWalletCharge), JsonSerializer.Serialize(actualEWalletCharge));
+        }
+
+        [Fact]
+        public async void EWalletCharge_ShouldSuccess_CreateParameters()
+        {
+            MockClient
+                .Setup(client => client.Request<EWalletCharge>(HttpMethod.Post, new Dictionary<string, string>(), Constant.EWalletChargeUrl, Constant.EWalletBody))
+                .ReturnsAsync(Constant.ExpectedEWalletCharge);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            EWalletCharge actualEWalletCharge = await EWalletCharge.Create(Constant.ReferenceId, Constant.Currency, Constant.Amount, Constant.CheckoutMethod, Constant.ChannelCode, Constant.ChannelProperties, null, null, null, null);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedEWalletCharge), JsonSerializer.Serialize(actualEWalletCharge));
+        }
+
+        [Fact]
+        public async void EWalletCharge_ShouldSuccess_CreateParameters_WithHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<EWalletCharge>(HttpMethod.Post, Constant.CustomHeaders, Constant.EWalletChargeUrl, Constant.EWalletBody))
+                .ReturnsAsync(Constant.ExpectedEWalletCharge);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            EWalletCharge actualEWalletCharge = await EWalletCharge.Create(Constant.CustomHeaders, Constant.ReferenceId, Constant.Currency, Constant.Amount, Constant.CheckoutMethod, Constant.ChannelCode, Constant.ChannelProperties, null, null, null, null);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedEWalletCharge), JsonSerializer.Serialize(actualEWalletCharge));
+        }
+    }
+}
