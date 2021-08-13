96f4b45125281bea71289abb606e73e53d99bae0
Shafiya Adzhani <adz.arsym@gmail.com>
2021-08-13 14:05:09 +0800

    feat(ewallets): add ewallet charge and basket model
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ Xendit.net/Xendit.net/Model/EWalletBasketItem.cs
@@ -0,0 +1,41 @@
+﻿namespace Xendit.net.Model
+{
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+
+    public class EWalletBasketItem
+    {
+        [JsonPropertyName("reference_id")]
+        public string ReferenceId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("category")]
+        public string Category { get; set; }
+
+        [JsonPropertyName("currency")]
+        public string Currency { get; set; }
+
+        [JsonPropertyName("price")]
+        public long Price { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public long Quantity { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("sub_category")]
+        public string SubCategory { get; set; }
+
+        [JsonPropertyName("metadata")]
+        public Dictionary<string, object> Metadata { get; set; }
+    }
+}
