deb2b83d2469820e5400ff7667975ed83512bf68
Shafiya Adzhani <adz.arsym@gmail.com>
2021-08-24 14:27:05 +0800

    test(payment methods): add payment methods test
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ Xendit.net/XenditTest/PaymentMethodTest/Constant.cs
@@ -0,0 +1,53 @@
+﻿namespace XenditTest.PaymentMethodTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xendit.net.Model;
+
+    internal class Constant
+    {
+        internal static readonly PaymentMethod ExpectedPaymentMethod = new PaymentMethod
+        {
+            Id = "payment-method-id",
+            Type = "DEBIT_CARD",
+            Properties = PaymentMethodBodyProperties,
+            CustomerId = "customer-id",
+            Status = "ACTIVE",
+            Created = "2020-03-19T05:34:55+0800",
+            Updated = "2020 - 03 - 19T05:24:55 + 0800",
+            Metadata = null,
+        };
+
+        internal static readonly PaymentMethod[] ExpectedPaymentMethods = new PaymentMethod[] { ExpectedPaymentMethod };
+
+        internal static readonly Dictionary<string, object> PaymentMethodBodyProperties = new Dictionary<string, object>()
+        {
+            { "id", "la-aa620619-124f-41db-995b-66a52abe036a" },
+            { "channel_code", "DC_BRI" },
+            { "currency", "IDR" },
+            { "card_last_four", "1234" },
+            { "card_expiry", "06/24" },
+            { "description", null },
+        };
+
+        internal static readonly Dictionary<string, object> PaymentMethodBody = new Dictionary<string, object>()
+        {
+            { "customer_id", "customer-id" },
+            { "type", "DEBIT_CARD" },
+            { "properties", PaymentMethodBodyProperties },
+        };
+
+        internal static readonly string CustomerId = "customer-id";
+
+        internal static readonly string PaymentMethodUrl = "https://api.xendit.co/payment_methods";
+        internal static readonly string GetPaymentMethodByCustomerIdUrl = string.Format("{0}{1}{2}", PaymentMethodUrl, "?customer_id=", CustomerId);
+
+        internal static readonly Dictionary<string, string> CustomHeaders = new Dictionary<string, string>()
+        {
+            { "for-user-id", "user-id" },
+        };
+    }
+}
---
+++ Xendit.net/XenditTest/PaymentMethodTest/PaymentMethodTest.cs
@@ -0,0 +1,68 @@
+﻿namespace XenditTest.PaymentMethodTest
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text.Json;
+    using Moq;
+    using Xendit.net;
+    using Xendit.net.Model;
+    using Xendit.net.Network;
+    using Xunit;
+
+    public class PaymentMethodTest
+    {
+        private static readonly Mock<INetworkClient> MockClient = new Mock<INetworkClient>();
+
+        [Fact]
+        public async void PaymentMethod_ShouldSuccess_CreatePaymentMethod()
+        {
+            MockClient
+                .Setup(client => client.Request<PaymentMethod>(HttpMethod.Post, new Dictionary<string, string>(), Constant.PaymentMethodUrl, Constant.PaymentMethodBody))
+                .ReturnsAsync(Constant.ExpectedPaymentMethod);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            PaymentMethod actualPaymentMethod = await PaymentMethod.Create(Constant.PaymentMethodBody);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedPaymentMethod), JsonSerializer.Serialize(actualPaymentMethod));
+        }
+
+        [Fact]
+        public async void PaymentMethod_ShouldSuccess_CreatePaymentMethod_WithHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<PaymentMethod>(HttpMethod.Post, Constant.CustomHeaders, Constant.PaymentMethodUrl, Constant.PaymentMethodBody))
+                .ReturnsAsync(Constant.ExpectedPaymentMethod);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            PaymentMethod actualPaymentMethod = await PaymentMethod.Create(Constant.PaymentMethodBody, Constant.CustomHeaders);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedPaymentMethod), JsonSerializer.Serialize(actualPaymentMethod));
+        }
+
+        [Fact]
+        public async void PaymentMethod_ShouldSuccess_GetPaymentMethodByCustomerId()
+        {
+            MockClient
+                .Setup(client => client.Request<PaymentMethod[]>(HttpMethod.Get, new Dictionary<string, string>(), Constant.GetPaymentMethodByCustomerIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedPaymentMethods);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            PaymentMethod[] actualPaymentMethods = await PaymentMethod.Get(Constant.CustomerId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedPaymentMethods), JsonSerializer.Serialize(actualPaymentMethods));
+        }
+
+        [Fact]
+        public async void PaymentMethod_ShouldSuccess_GetPaymentMethodByCustomerId_WthHeaders()
+        {
+            MockClient
+                .Setup(client => client.Request<PaymentMethod[]>(HttpMethod.Get, Constant.CustomHeaders, Constant.GetPaymentMethodByCustomerIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedPaymentMethods);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            PaymentMethod[] actualPaymentMethods = await PaymentMethod.Get(Constant.CustomerId, Constant.CustomHeaders);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedPaymentMethods), JsonSerializer.Serialize(actualPaymentMethods));
+        }
+    }
+}
