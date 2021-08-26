dc8fc87925dd7b2241828a7e541eb185f44d4a68
Shafiya Adzhani <adz.arsym@gmail.com>
2021-08-26 10:37:41 +0800

    fix(invoice): fix using in example get invoice
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

--- XenditExample/ExampleGetInvoice.cs
+++ XenditExample/ExampleGetInvoice.cs
@@ -1,38 +1,39 @@
 namespace XenditExample
 {
-	using System;
-	using System.Threading.Tasks;
-	using System.Net.Http;
-	using Xendit.net;
-	using Xendit.net.Exception;
-	using Xendit.net.Model;
-	using Xendit.net.Network;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Net.Http;
+    using Xendit.net;
+    using Xendit.net.Exception;
+    using Xendit.net.Model;
+    using Xendit.net.Network;
 
-	class ExampleGetInvoice
-	{
-		static async Task Main(string[] args)
-		{
-			HttpClient httpClient = new HttpClient();
-			NetworkClient networkClient = new NetworkClient(httpClient);
-			XenditConfiguration.RequestClient = networkClient;
-			XenditConfiguration.ApiKey = "xnd_development_...";
+    class ExampleGetInvoice
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            NetworkClient networkClient = new NetworkClient(httpClient);
+            XenditConfiguration.RequestClient = networkClient;
+            XenditConfiguration.ApiKey = "xnd_development_...";
 
-			try
-			{
-				string invoiceId = "invoice_id";
-				Invoice invoice = await Invoice.GetById(invoiceId);
-				Console.WriteLine(invoice);
+            try
+            {
+                string invoiceId = "invoice_id";
+                Invoice invoice = await Invoice.GetById(invoiceId);
+                Console.WriteLine(invoice);
 
-				Dictionary<string, object> parameter = new Dictionary<string, object>();
-				parameter.Add("limit", 1);
-				parameter.Add("statutes", "[\"PENDING\",\"EXPIRED\"]");
-				Invoice[] invoiceArray = await Invoice.GetAll(parameter);
-				Console.WriteLine(invoiceArray);
-			}
-			catch (XenditException e)
-			{
-				Console.WriteLine(e.ToString());
-			}
-		}
-	}
+                Dictionary<string, object> parameter = new Dictionary<string, object>();
+                parameter.Add("limit", 1);
+                parameter.Add("statutes", "[\"PENDING\",\"EXPIRED\"]");
+                Invoice[] invoiceArray = await Invoice.GetAll(parameter);
+                Console.WriteLine(invoiceArray);
+            }
+            catch (XenditException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
 }
