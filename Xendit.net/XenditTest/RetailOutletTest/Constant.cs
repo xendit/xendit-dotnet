e8526fb7e717ec59f228e5d3881171e30954fa6e
Shafiya Adzhani <adz.arsym@gmail.com>
2021-09-08 05:37:05 +0800

    test(retail outlet): add test for retail outlet
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ Xendit.net/XenditTest/RetailOutletTest/Constant.cs
@@ -0,0 +1,67 @@
+﻿namespace XenditTest.RetailOutletTest
+{
+    using System.Collections.Generic;
+    using Xendit.net.Enum;
+    using Xendit.net.Struct;
+    using RetailOutlet = Xendit.net.Model.RetailOutlet;
+
+    internal class Constant
+    {
+        internal static readonly string RetailOutletUrl = "https://api.xendit.co/payment_codes";
+        internal static readonly CreateRetailOutletParameter CreateRetailOutletParameter = new CreateRetailOutletParameter
+        {
+            ReferenceId = "reference_id",
+            ChannelCode = RetailOutletEnum.ChannelCode.SevenEleven,
+            Amount = 1000,
+            Currency = RetailOutletEnum.Currency.PHP,
+            CustomerName = "Rika Sutanto",
+            Market = RetailOutletEnum.Country.Philippines,
+            ExpiresAt = "2021-05-30T02:38:01.327283048Z",
+            IsSingleUse = false,
+            Description = "Example of payment code",
+            Metadata = null,
+        };
+
+        internal static readonly RetailOutlet ExpectedRetailOutlet = new RetailOutlet
+        {
+            Id = "pcode-559bb2df-6ae7-4fb8-bc05-f17aa07aeae1",
+            BusinessId = "5f21361959ef2b788cbbe97f",
+            ReferenceId = "reference_id",
+            CustomerName = "Rika Sutanto",
+            PaymentCode = "TEST906558",
+            Currency = RetailOutletEnum.Currency.PHP,
+            Amount = 1000,
+            IsSingleUse = false,
+            ChannelCode = RetailOutletEnum.ChannelCode.SevenEleven,
+            Description = "Example of payment code",
+            Market = RetailOutletEnum.Country.Philippines,
+            Status = RetailOutletEnum.Status.Active,
+            Metadata = null,
+            CreatedAt = "2021-05-30T02:38:01.327283048Z",
+            UpdatedAt = "2021-05-30T02:38:01.327283048Z",
+            ExpiresAt = "2021-05-30T02:38:01.327283048Z",
+        };
+
+        internal static readonly RetailOutlet[] ExpectedRetailOutlets = new RetailOutlet[]
+        {
+            ExpectedRetailOutlet,
+        };
+
+        internal static readonly UpdateRetailOutletParameter UpdateRetailOutletParameter = new UpdateRetailOutletParameter
+        {
+            CustomerName = "Rika Sutanto",
+            Amount = 1000,
+            Currency = RetailOutletEnum.Currency.PHP,
+            ExpiresAt = "2021-05-30T02:38:01.327283048Z",
+            Description = "Example of payment code",
+        };
+
+        internal static readonly string PaymentCodeId = "payment_code_id";
+        internal static readonly string PaymentCodeIdUrl = string.Format("{0}/{1}", RetailOutletUrl, PaymentCodeId);
+        internal static readonly string GetPaymentsUrl = string.Format("{0}/payments", PaymentCodeIdUrl);
+        internal static readonly Dictionary<string, string> Headers = new Dictionary<string, string>()
+        {
+            { "for-user-id", "user-id" },
+        };
+    }
+}
---
+++ Xendit.net/XenditTest/RetailOutletTest/RetailOutletTest.cs
@@ -0,0 +1,82 @@
+﻿namespace XenditTest.RetailOutletTest
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text.Json;
+    using Moq;
+    using Xendit.net;
+    using Xendit.net.Model;
+    using Xendit.net.Network;
+    using Xendit.net.Struct;
+    using Xunit;
+
+    public class RetailOutletTest
+    {
+        private static readonly Mock<INetworkClient> MockClient = new Mock<INetworkClient>();
+
+        [Fact]
+        public async void RetailOutlet_Create_ShouldSuccess()
+        {
+            MockClient
+                .Setup(client => client.Request<CreateRetailOutletParameter, RetailOutlet>(HttpMethod.Post, new Dictionary<string, string>(), Constant.RetailOutletUrl, Constant.CreateRetailOutletParameter))
+                .ReturnsAsync(Constant.ExpectedRetailOutlet);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            RetailOutlet actualRetailOutlet = await RetailOutlet.Create(Constant.CreateRetailOutletParameter);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedRetailOutlet), JsonSerializer.Serialize(actualRetailOutlet));
+        }
+
+        [Fact]
+        public async void RetailOutlet_CreateWithHeader_ShouldSuccess()
+        {
+            MockClient
+                .Setup(client => client.Request<CreateRetailOutletParameter, RetailOutlet>(HttpMethod.Post, Constant.Headers, Constant.RetailOutletUrl, Constant.CreateRetailOutletParameter))
+                .ReturnsAsync(Constant.ExpectedRetailOutlet);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            RetailOutlet actualRetailOutlet = await RetailOutlet.Create(Constant.CreateRetailOutletParameter, Constant.Headers);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedRetailOutlet), JsonSerializer.Serialize(actualRetailOutlet));
+        }
+
+        [Fact]
+        public async void RetailOutlet_Update_ShouldSuccess()
+        {
+            MockClient
+                .Setup(client => client.Request<UpdateRetailOutletParameter, RetailOutlet>(XenditHttpMethod.Patch, new Dictionary<string, string>(), Constant.PaymentCodeIdUrl, Constant.UpdateRetailOutletParameter))
+                .ReturnsAsync(Constant.ExpectedRetailOutlet);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            RetailOutlet actualRetailOutlet = await RetailOutlet.Update(Constant.UpdateRetailOutletParameter, Constant.PaymentCodeId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedRetailOutlet), JsonSerializer.Serialize(actualRetailOutlet));
+        }
+
+        [Fact]
+        public async void RetailOutlet_GetPaymentCode_ShouldSuccess()
+        {
+            MockClient
+                .Setup(client => client.Request<Dictionary<string, string>, RetailOutlet>(HttpMethod.Get, new Dictionary<string, string>(), Constant.PaymentCodeIdUrl, null))
+                .ReturnsAsync(Constant.ExpectedRetailOutlet);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            RetailOutlet actualRetailOutlet = await RetailOutlet.GetPaymentCode(Constant.PaymentCodeId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedRetailOutlet), JsonSerializer.Serialize(actualRetailOutlet));
+        }
+
+        [Fact]
+        public async void RetailOutlet_GetPayments_ShouldSuccess()
+        {
+            MockClient
+                .Setup(client => client.Request<Dictionary<string, string>, RetailOutlet[]>(HttpMethod.Get, new Dictionary<string, string>(), Constant.GetPaymentsUrl, null))
+                .ReturnsAsync(Constant.ExpectedRetailOutlets);
+
+            XenditConfiguration.RequestClient = MockClient.Object;
+
+            RetailOutlet[] actualRetailOutlet = await RetailOutlet.GetPayments(Constant.PaymentCodeId);
+            Assert.Equal(JsonSerializer.Serialize(Constant.ExpectedRetailOutlets), JsonSerializer.Serialize(actualRetailOutlet));
+        }
+    }
+}
