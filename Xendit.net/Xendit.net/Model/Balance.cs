a57aef5c45ba7b0dbd1ba5540a8191732fd417a4
Shafiya Adzhani <adz.arsym@gmail.com>
2021-09-14 22:44:24 +0800

    refactor(balance): refactor to use enum and fix network client

--- Xendit.net/Xendit.net/Model/Balance.cs
+++ Xendit.net/Xendit.net/Model/Balance.cs
@@ -1,104 +1,39 @@
 ﻿namespace Xendit.net.Model
 {
-    using System;
-    using System.Collections.Generic;
     using System.Net.Http;
+    using System.Text.Json;
     using System.Text.Json.Serialization;
     using System.Threading.Tasks;
-    using Xendit.net.Exception;
+    using Xendit.net.Enum;
+    using Xendit.net.Struct;
 
     public class Balance
     {
-        public enum AccountType
-        {
-            Cash,
-            Holding,
-            Tax,
-        }
-
         [JsonPropertyName("balance")]
-        public int Value { get; set; }
-
-        /// <summary>
-        /// Get balance from your account.
-        /// </summary>
-        /// <returns>A Task that contains balance.</returns>
-        public static async Task<Balance> Get()
-        {
-            return await GetBalanceAsync(new Dictionary<string, string>(), null);
-        }
+        public long Value { get; set; }
 
         /// <summary>
         /// Get balance from your account based on given account type.
         /// </summary>
         /// <param name="accountType">Selected balance type (in enum).</param>
-        /// <returns>A Task that contains balance.</returns>
-        public static async Task<Balance> Get(AccountType accountType)
+        /// <param name="headers">Custom headers <see cref="HeaderParameter"/>. Use property based on <seealso href="https://developers.xendit.co/api-reference/#get-balance"/>.</param>
+        /// <returns>A Task of <see cref="Balance"/>.</returns>
+        public static async Task<Balance> Get(BalanceAccountType? accountType = null, HeaderParameter? headers = null)
         {
-            return await GetBalanceAsync(new Dictionary<string, string>(), accountType);
+            return await GetBalanceRequest(accountType, headers);
         }
 
-        /// <summary>
-        /// Get balance from your account based on given account type string (Cash, Holding, Tax).
-        /// </summary>
-        /// <param name="accountTypeValue">Selected balance type in string ("Cash", "Holding", "Tax").</param>
-        /// <exception cref="ParamException">Thrown when account type value is not "Cash", "Holding", "Tax".</exception>
-        /// <returns>A Task that contains balance.</returns>
-        public static async Task<Balance> Get(string accountTypeValue)
-        {
-            try
-            {
-                AccountType accountType = (AccountType)Enum.Parse(typeof(AccountType), accountTypeValue);
-                return await GetBalanceAsync(new Dictionary<string, string>(), accountType);
-            }
-            catch (ArgumentException)
-            {
-                throw new ParamException("Value should be Cash, Holding, or Tax");
-            }
-        }
-
-        /// <summary>
-        /// Get balance from your account based on given account type with custom headers.
-        /// </summary>
-        /// <param name="headers">Custom headers. e.g: "for-user-id".</param>
-        /// <param name="accountType">Selected balance type (in enum).</param>
-        /// <returns>A Task that contains balance.</returns>
-        public static async Task<Balance> Get(Dictionary<string, string> headers, AccountType accountType)
-        {
-            return await GetBalanceAsync(headers, accountType);
-        }
-
-        /// <summary>
-        /// Get balance from your account based on given account type string (Cash, Holding, Tax) with custom headers.
-        /// </summary>
-        /// <param name="headers">Custom headers. e.g: "for-user-id".</param>
-        /// <param name="accountTypeValue">Selected balance type in string ("Cash", "Holding", "Tax").</param>
-        /// <exception cref="ArgumentException">Thrown when account type value is not "Cash", "Holding", "Tax".</exception>
-        /// <returns>A Task that contains balance.</returns>
-        public static async Task<Balance> Get(Dictionary<string, string> headers, string accountTypeValue)
-        {
-            try
-            {
-                AccountType accountType = (AccountType)Enum.Parse(typeof(AccountType), accountTypeValue);
-                return await GetBalanceAsync(headers, accountType);
-            }
-            catch (ArgumentException)
-            {
-                throw new ParamException("Value should be Cash, Holding, or Tax");
-            }
-        }
-
-        private static async Task<Balance> GetBalanceAsync(Dictionary<string, string> headers, AccountType? accountType)
+        private static async Task<Balance> GetBalanceRequest(BalanceAccountType? accountType, HeaderParameter? headers)
         {
             string url = string.Format("{0}{1}", XenditConfiguration.ApiUrl, "/balance");
 
             if (accountType != null)
             {
-                string accountTypeParam = accountType.ToString().ToUpper();
+                string accountTypeParam = JsonSerializer.Deserialize<string>(JsonSerializer.Serialize(accountType));
                 url = string.Format("{0}{1}{2}", url, "?account_type=", accountTypeParam);
             }
 
-            var balance = await XenditConfiguration.RequestClient.Request<Balance>(HttpMethod.Get, headers, url, null);
+            var balance = await XenditConfiguration.RequestClient.Request<Balance>(HttpMethod.Get, headers, url);
             return balance;
         }
     }
