61dfb0946343fae634cdb79b74d5ceab63054fef
Shafiya Adzhani <adz.arsym@gmail.com>
2021-09-22 12:20:22 +0800

    docs(virtual account): fix update virtual account example
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

--- XenditExample/ExampleUpdateVirtualAccount.cs
+++ XenditExample/ExampleUpdateVirtualAccount.cs
@@ -27,7 +27,7 @@ namespace XenditExample
                     ExpectedAmount = 20000,
                 };
 
-                VirtualAccount virtualAccount = await VirtualAccount.Update(parameter, "virtual_account_id");
+                VirtualAccountResponse virtualAccount = await VirtualAccount.Update(parameter, "virtual_account_id");
                 Console.WriteLine(virtualAccount);
             }
             catch (XenditException e)
